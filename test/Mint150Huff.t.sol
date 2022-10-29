c3d34d26c089c8429ed4e51c0f601b3d7cbf4e80
kadenzipfel <kaden.zipfel@hotmail.com>
2022-10-29 10:04:28 -0700

    Setup huff test

---
+++ test/Mint150Huff.t.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
+import {NotRareToken, OptimizedAttacker} from "../src/Mint150.sol";
+
+contract Mint150Test is Test {
+    NotRareToken nrt;
+    uint256 constant PREMINTS = 5;
+
+    function setUp() external {
+        nrt = new NotRareToken();
+        for (uint256 i; i < PREMINTS; ++i) {
+            vm.prank(address(1));
+            nrt.mint();
+        }
+    }
+
+    function testMint150() external {
+        vm.prank(address(0xBEEF));
+        HuffDeployer
+            .config()
+            .with_args(abi.encode(address(nrt)))
+            .deploy('../src/Mint150');
+
+        uint256 balance = nrt.balanceOf(address(0xBEEF));
+        assertEq(balance, 150);
+    }
+}
