cc61b048188c0d89e9ccc4c99a8d04a380d5be12
kadenzipfel <kaden.zipfel@hotmail.com>
2022-10-29 11:41:49 -0700

    testGetVictim

--- test/Mint150Huff.t.sol
+++ test/Mint150Huff.t.sol
@@ -1,10 +1,15 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
+import {HuffConfig} from "foundry-huff/HuffConfig.sol";
 import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
 import {NotRareToken, OptimizedAttacker} from "../src/Mint150.sol";
 
-contract Mint150Test is Test {
+interface Mint150 {
+    function getVictim() view external returns (address);
+}
+
+contract Mint150HuffTest is Test {
     NotRareToken nrt;
     uint256 constant PREMINTS = 5;
 
@@ -16,14 +21,25 @@ contract Mint150Test is Test {
         }
     }
 
-    function testMint150() external {
+    function testGetVictim() external {
+        string memory wrapper_code = vm.readFile("test/Mint150Wrapper.huff");
+        HuffConfig config = HuffDeployer.config()
+            .with_code(wrapper_code)
+            .with_args(abi.encode(address(nrt)));
         vm.prank(address(0xBEEF));
-        HuffDeployer
-            .config()
-            .with_args(abi.encode(address(nrt)))
-            .deploy('../src/Mint150');
+        Mint150 mint150 = Mint150(config.deploy('../src/Mint150'));
 
-        uint256 balance = nrt.balanceOf(address(0xBEEF));
-        assertEq(balance, 150);
+        address victim = Mint150(mint150).getVictim();
     }
+
+    // function testMint150() external {
+    //     vm.prank(address(0xBEEF));
+    //     HuffDeployer
+    //         .config()
+    //         .with_args(abi.encode(address(nrt)))
+    //         .deploy('../src/Mint150');
+
+    //     uint256 balance = nrt.balanceOf(address(0xBEEF));
+    //     assertEq(balance, 150);
+    // }
 }
